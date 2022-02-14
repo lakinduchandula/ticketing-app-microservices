f4fb15740792bf2915913d41c40687f3ac4bbae4
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-02-14 23:05:56 +0530

    implement routing for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,13 +1,20 @@
 import express from "express";
 import { json } from "body-parser";
 
+// Routing Imports
+import { currentUserRouter } from "./routes/current-user";
+import { signUpUserRouter } from "./routes/signup";
+import { signInUserRouter } from "./routes/signin";
+import { signOutUserRouter } from "./routes/signout";
+
 const app = express();
 
 app.use(json());
 
-app.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
-});
+app.use(currentUserRouter);
+app.use(signInUserRouter);
+app.use(signOutUserRouter);
+app.use(signUpUserRouter);
 
 app.listen(3000, () => {
   console.log("==>> Listening on port 3000 <<==");

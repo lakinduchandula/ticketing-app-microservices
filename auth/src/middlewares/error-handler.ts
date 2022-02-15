c74a151e40d54d75e67f5eb28e43fb0ac88f878e
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-02-15 12:56:15 +0530

    format error

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,6 @@
 import { Response, Request, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 export const errorHandler = (
   err: Error,
@@ -7,6 +9,16 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   //console.log("Something went wrong!", err.message);
+  if (err instanceof RequestValidationError) {
+    const formattedErrors = err.errors.map(error => {
+      return { message: error.msg, field: error.param };
+    });
+    res.status(400).send({ errors: formattedErrors });
+  }
+
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(500).send({ errors: [{ message: err.reason }] });
+  }
 
   res.status(400).send({
     message: err.message,

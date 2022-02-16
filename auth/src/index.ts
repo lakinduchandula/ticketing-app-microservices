c62572fd23f8919effd9aba4a77e8893d4661bcd
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-02-16 08:44:18 +0530

    mongodb connection error was resolved!

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "express-async-errors";
 import { json } from "body-parser";
+import mongoose from "mongoose";
 
 // Routing Imports
 import { currentUserRouter } from "./routes/current-user";
@@ -25,6 +26,20 @@ app.all("*", async () => {
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("==>> Listening on port 3000 <<==");
-});
+const start = async () => {
+  try {
+    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("==>> Connected to MongoDB");
+  } catch (err) {
+    console.log("Error Pop-up ==> " + err);
+  }
+
+  app.listen(3000, () => {
+    console.log("==>> Listening on port 3000 <<==");
+  });
+};
+
+start();

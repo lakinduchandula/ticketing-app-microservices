6ad064bbf8167abbd582a84121d8513be3d95bcf
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-02-19 10:16:03 +0530

    index to app refactor

---
+++ auth/src/app.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+
+// Routing Imports
+import { currentUserRouter } from "./routes/current-user";
+import { signUpUserRouter } from "./routes/signup";
+import { signInUserRouter } from "./routes/signin";
+import { signOutUserRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
+
+const app = express();
+
+/* Traffic has been proxied to our application through ingress-nginx, 
+ * express gonna see the fact that traffic has been proxied and by default,
+ * express gonna say 'I don't really trust this https connection'
+ ** Adding this app.set(...) to make sure that express is aware behind the proxy (ingress-nginx)
+    And to make sure this traffic even though it is comming from that proxy
+*/
+app.set("trust proxy", true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // disable encryption on this cookie
+    secure: true, // only be use if user visiting our website throug => https://
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signInUserRouter);
+app.use(signOutUserRouter);
+app.use(signUpUserRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };

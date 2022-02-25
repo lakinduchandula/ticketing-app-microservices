b8c6a635d9fc729e42b0665c91d227a9f004db75
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-02-25 19:46:13 +0530

    server side rendering

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,13 @@
-export default () => {
+const LandingPage = ({ color }) => {
+  console.log('I am in the Component', color);
+
   return <h1>Landing Page</h1>;
 };
+
+LandingPage.getInitialProps = () => {
+  console.log('I am on the Server!');
+
+  return { color: 'red' };
+};
+
+export default LandingPage;

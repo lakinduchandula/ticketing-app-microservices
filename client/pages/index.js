738425a16b6f78013c641e12ee480fc998cf8f4d
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-02-26 14:10:35 +0530

    attach the headers to endpoint ingress-nginx

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,15 +6,18 @@ const LandingPage = ({ currentUser }) => {
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+/* { req } => in here equal to node(express) endpoint req there for it has headers and other attributes*/
+LandingPage.getInitialProps = async ({ req }) => {
+  console.log(req.headers);
   if (typeof window === 'undefined') {
     // we are on the server!
     // request should be made to;
     // => http://NAME_OF_SERVICE.NAMESPACE.svc.cluster.local
     // => http://ingress-nginx.ingress-nginx-controller.svc.cluster.local
     const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-        headers: { Host: 'ticketing.dev'}
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+      {
+        headers: req.headers,
       }
     );
     return data;

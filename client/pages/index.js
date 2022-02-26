4676993916e6adb51f01635469b4f796bc8005b7
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-02-26 14:03:17 +0530

    getInitialProps exeuting when browser vs pod

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,29 @@
-const LandingPage = ({ color }) => {
-  console.log('I am in the Component', color);
+import axios from 'axios';
 
+const LandingPage = ({ currentUser }) => {
+  console.log('Am I logged in ?', currentUser);
+  // axios.get('/api/users/currentuser');
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = () => {
-  console.log('I am on the Server!');
-
-  return { color: 'red' };
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // we are on the server!
+    // request should be made to;
+    // => http://NAME_OF_SERVICE.NAMESPACE.svc.cluster.local
+    // => http://ingress-nginx.ingress-nginx-controller.svc.cluster.local
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+        headers: { Host: 'ticketing.dev'}
+      }
+    );
+    return data;
+  } else {
+    // we are on the browser
+    // request can be made with a base url of ''
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
 };
 
 export default LandingPage;

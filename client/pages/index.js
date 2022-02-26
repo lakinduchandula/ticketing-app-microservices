25e23f2edd8fb88cb9ee6ccc60c2491649177df4
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-02-26 16:02:00 +0530

    reusable API Client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
   console.log('Am I logged in ?', currentUser);
@@ -7,26 +8,10 @@ const LandingPage = ({ currentUser }) => {
 };
 
 /* { req } => in here equal to node(express) endpoint req there for it has headers and other attributes*/
-LandingPage.getInitialProps = async ({ req }) => {
-  console.log(req.headers);
-  if (typeof window === 'undefined') {
-    // we are on the server!
-    // request should be made to;
-    // => http://NAME_OF_SERVICE.NAMESPACE.svc.cluster.local
-    // => http://ingress-nginx.ingress-nginx-controller.svc.cluster.local
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-      {
-        headers: req.headers,
-      }
-    );
-    return data;
-  } else {
-    // we are on the browser
-    // request can be made with a base url of ''
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
+LandingPage.getInitialProps = async context => {
+  const client = buildClient(context)
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;

88fbab19e92c10d7c487fae106f3992bbcfb2622
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-02-26 16:06:11 +0530

    leave the msg to usr signed in or not

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,15 +1,16 @@
-import axios from 'axios';
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log('Am I logged in ?', currentUser);
-  // axios.get('/api/users/currentuser');
-  return <h1>Landing Page</h1>;
+  return currentUser ? (
+    <h1>You are signed In</h1>
+  ) : (
+    <h1>You are not signed In</h1>
+  );
 };
 
 /* { req } => in here equal to node(express) endpoint req there for it has headers and other attributes*/
 LandingPage.getInitialProps = async context => {
-  const client = buildClient(context)
+  const client = buildClient(context);
   const { data } = await client.get('/api/users/currentuser');
   return data;
 };

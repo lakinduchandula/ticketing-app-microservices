87346c14b93c880822921c4ac290e4daac29dfd0
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-02-28 13:51:11 +0530

    passing Props Through

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,8 +10,10 @@ const LandingPage = ({ currentUser }) => {
 
 /* { req } => in here equal to node(express) endpoint req there for it has headers and other attributes*/
 LandingPage.getInitialProps = async context => {
+  console.log('LANDING PAGE!');
   const client = buildClient(context);
   const { data } = await client.get('/api/users/currentuser');
+
   return data;
 };
 

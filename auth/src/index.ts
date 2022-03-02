24559a543c15bf1c4dbbabc8f81c93ee62b74873
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-02 13:47:14 +0530

    update MONGO_URI as env in auth and tickets

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,25 +1,29 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-import { app } from "./app";
+import { app } from './app';
 
 const start = async () => {
   // check before application start environment variables get defined correctly
   if (!process.env.JWT_KEY) {
-    throw new Error("JWT_KEY is not defined");
+    throw new Error('JWT_KEY is not defined');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI is not defined');
   }
 
   try {
-    await mongoose.connect("mongodb://auth-mongo-srv:27017/auth", {
+    await mongoose.connect(process.env.MONGO_URI!, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("==>> Connected to MongoDB");
+    console.log('==>> Connected to MongoDB');
   } catch (err) {
-    console.log("Error Pop-up ==> " + err);
+    console.log('Error Pop-up ==> ' + err);
   }
 
   app.listen(3000, () => {
-    console.log("==>> Listening on port 3000 <<==");
+    console.log('==>> Listening on port 3000 <<==');
   });
 };
 

1b1b612c10763e1c152f7653a9c30c3ccfc013ba
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-03 14:22:39 +0530

    /api/tickets endpoint test and validate

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -9,8 +9,14 @@ it('has a route handler listening to /api/tickets for post requests', async () =
 it('can only be accessed if the user is signed in', async () => {});
 
 it('returns an error if an invalid title is provided', async () => {
-  const response = await request(app).post('/api/tickets').send({});
-  expect(response.status).toEqual(401);
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'ngkJghLPh3gF7GL',
+      price: -10,
+    })
+    .expect(400);
 });
 
 it('returns a status other than 401 if the user is signed in', async () => {
@@ -22,6 +28,14 @@ it('returns a status other than 401 if the user is signed in', async () => {
   expect(response.status).not.toEqual(401);
 });
 
-it('returns an error if an invalid price is provided', async () => {});
+it('returns an error if an invalid price is provided', async () => {
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+     title: 'TbGt5yPKVw9TOjQ',
+    })
+    .expect(400);
+});
 
 it('creates a tickets with valid inputs', async () => {});
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@lc-tickets/common';
+
+import { body } from 'express-validator';
+import { requireAuth, validateRequest } from '@lc-tickets/common';
 
 const router = express.Router();
 
-router.post('/api/tickets', requireAuth, (req: Request, res: Response) => {
-  res.sendStatus(200);
-});
+router.post(
+  '/api/tickets',
+  requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .not()
+      .isEmpty()
+      .isFloat({ gt: 0 })
+      .withMessage('Price sholud be valid'),
+  ],
+  validateRequest,
+  (req: Request, res: Response) => {
+    res.sendStatus(200);
+  }
+);
 
 export { router as createTicketRouter };

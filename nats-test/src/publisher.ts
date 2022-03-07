8103e7cdd158c4d5e8be42aa520cf24414352c40
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-07 12:12:20 +0530

    listener basic implementation

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,7 @@
 import nats from 'node-nats-streaming';
 
+console.clear(); // clear the console
+
 // stan is a actual instance or client that we are using connect to nats-streaming server
 const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',

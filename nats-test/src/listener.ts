9efa1c6ce025bce13e26f0646881e75fc02dc4ed
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-07 18:52:09 +0530

    create a queue group

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,23 @@
 import nats, { Message } from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
 
 console.clear(); // clear the console
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 });
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
-  const subscription = stan.subscribe('ticket:created');
+  /**
+   * Queue group is created to make-sure that Multiple instances in the same service 
+   * are not all going to receive the exact same event
+   */
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'order-service-queue-group'
+  );
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData();

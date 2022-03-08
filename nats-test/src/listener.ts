e02ed11f7072480abdb3255f93a77fa0fcf5a408
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-08 14:00:10 +0530

    closing connection

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -10,6 +10,11 @@ const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
 stan.on('connect', () => {
   console.log('Listener connected to NATS');
 
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
   const options = stan.subscriptionOptions().setManualAckMode(true);
   /**
    * Queue group is created to make-sure that Multiple instances in the same service
@@ -30,4 +35,19 @@ stan.on('connect', () => {
 
     msg.ack();
   });
+});
+
+/**
+ * These are watching for interupt signals or terminate signals that are send
+ * to this process any time ts-node-dev restart out program and any-time we hit
+ * ctrl+c inside terminal
+ */
+process.on('SIGINT', () => {
+  // intercept
+  stan.close(); // call to close in line 13
+});
+
+process.on('SIGTERM', () => {
+  // terminate
+  stan.close(); // call to close in line 13
 });

0db2652c89a0a7832aeb8637c24183215b7e5f1d
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-10 14:43:48 +0530

    interface ticket-created-**

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -1,11 +1,17 @@
 import { Listener } from './base-listener';
 import { Message } from 'node-nats-streaming';
+import { TicketCreatedEvent } from './ticket-created-event';
+import { Subjects } from './subjects';
 
-export class TicketCreatedListener extends Listener {
-  subject = 'ticket:created';
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  /**
+   * In here "subject" variable without type annotation generate an error
+   * beacuse TS think that we may change the value of "subjects" in future
+   */
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
   queueGroupName = 'payment-service';
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
     console.log('Evenet data!', data);
 
     msg.ack(); // this indicates that msg successfully having parsed

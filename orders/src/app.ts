337e3b9697e53a88355db44a0d11366f35602cc5
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-14 17:49:37 +0530

    create and initialize orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { errorHandler, NotFoundError, currentUser } from '@lc-tickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+
+/* Traffic has been proxied to our application through ingress-nginx, 
+ * express gonna see the fact that traffic has been proxied and by default,
+ * express gonna say 'I don't really trust this https connection'
+ ** Adding this app.set(...) to make sure that express is aware behind the proxy (ingress-nginx)
+    And to make sure this traffic even though it is comming from that proxy
+*/
+app.set('trust proxy', true);
+
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false, // disable encryption on this cookie
+    // secure: true, // only be use if user visiting our website throug => https://
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ orders/src/index.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+
+import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  // check before application start environment variables get defined correctly
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY is not defined');
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error('MONGO_URI is not defined');
+  }
+
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL is not defined');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID is not defined');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID is not defined');
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => {
+      // intercept
+      natsWrapper.client.close(); // call to close in line 13
+    });
+
+    process.on('SIGTERM', () => {
+      // terminate
+      natsWrapper.client.close(); // call to close in line 13
+    });
+
+    await mongoose.connect(process.env.MONGO_URI!, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('==>> Connected to MongoDB');
+  } catch (err) {
+    console.log('Error Pop-up ==> ' + err);
+  }
+
+  app.listen(3000, () => {
+    console.log('==>> Listening on port 3000 <<==');
+  });
+};
+
+start();

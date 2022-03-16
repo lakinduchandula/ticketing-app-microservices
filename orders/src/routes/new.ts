21579b3b70fa85df95bebeea99faff24c8fdc81a
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-16 12:16:21 +0530

    build-up new order route handler

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,10 +1,22 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@lc-tickets/common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
 router.post(
   '/api/orders',
-  async (req, res) => (req: Request, res: Response) => {
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input)) // this will check whether "input" is valid mongo_id or not
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
     res.send({});
   }
 );

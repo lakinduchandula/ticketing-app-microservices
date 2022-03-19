545d1297f057966d08f34bdd5e75174754b894d4
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-19 17:17:06 +0530

    show order route handler

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,11 +1,28 @@
 import express, { Request, Response } from 'express';
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  requireAuth,
+} from '@lc-tickets/common';
+import { Order } from '../models/order';
 
 const router = express.Router();
 
 router.get(
   '/api/orders/:orderId',
-  async (req, res) => (req: Request, res: Response) => {
-    res.send({});
+  requireAuth,
+  async (req, res) => async (req: Request, res: Response) => {
+    const order = await Order.findById(req.params.orderId).populate('ticket');
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId === req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+
+    res.send(order);
   }
 );
 

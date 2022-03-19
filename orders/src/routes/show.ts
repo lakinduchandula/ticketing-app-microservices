315eefe64f4829fa5ad4414ad6c8f28932f5ce4f
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-19 17:25:00 +0530

    delete order

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -18,7 +18,7 @@ router.get(
       throw new NotFoundError();
     }
 
-    if (order.userId === req.currentUser!.id) {
+    if (order.userId !== req.currentUser!.id) {
       throw new NotAuthorizedError();
     }
 

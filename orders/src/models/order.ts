ce51e5c149a28f2b185e7a58e64ba4ede0c6d5ca
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-03-31 09:13:17 +0530

    add version property

--- orders/src/models/order.ts
+++ orders/src/models/order.ts
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 import { OrderStatus } from '@lc-tickets/common';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
+
 import { TicketDoc } from './ticket';
 
 interface OrderAttrs {
@@ -50,6 +52,9 @@ const orderSchema = new mongoose.Schema(
     },
   }
 );
+
+orderSchema.set('versionKey', 'version');
+orderSchema.plugin(updateIfCurrentPlugin);
 
 orderSchema.statics.build = (attrs: OrderAttrs) => {
   return new Order(attrs);
--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -16,6 +16,7 @@ it('returns an error if the ticket does not exist', async () => {
 
 it('returns an error if the ticket is already reserved', async () => {
   const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
     title: 'One Direction',
     price: 659,
   });
@@ -40,6 +41,7 @@ it('returns an error if the ticket is already reserved', async () => {
 
 it('reserves a ticket', async () => {
   const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
     title: 'Adam Levine',
     price: 399,
   });

ff6780a7dc97b9404515cfd8cbf7fb2e63ca25c3
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-04-09 14:47:20 +0530

    update ticket when someone reserved it test

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 import { app } from '../../app';
 import { natsWrapper } from '../../nats-wrapper';
+import { Ticket } from '../../models/ticket';
 
 it('returns a 404 if the provided id does not exsit', async () => {
   const id = new mongoose.Types.ObjectId().toHexString();
@@ -103,6 +104,33 @@ it('publishes an event', async () => {
       price: 100.0,
     })
     .expect(200);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('rejects updates if the ticket is reserved', async () => {
+  const cookie = global.signin();
+
+  const response = await request(app)
+    .post('/api/tickets')
+    .set('Cookie', cookie)
+    .send({
+      title: 'ngkJghLPh3gF7GL',
+      price: 10.75,
+    });
+
+  const ticket = await Ticket.findById(response.body.id);
+  ticket!.set({ orderId: mongoose.Types.ObjectId().toHexString() });
+  await ticket!.save();
+
+  await request(app)
+    .put(`/api/tickets/${response.body.id}`)
+    .set('Cookie', cookie)
+    .send({
+      title: 'tdwQOA3ffwmU2Jm',
+      price: 100.0,
+    })
+    .expect(400);
 
   expect(natsWrapper.client.publish).toHaveBeenCalled();
 });

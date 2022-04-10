e468f15f753c379608128111ce3e047d819a756a
Lakindu Chandula <lakindu.chandula@hotmail.com>
2022-04-10 09:12:14 +0530

    initialize expiration service

---
+++ expiration/src/index.ts
@@ -0,0 +1,43 @@
+import { natsWrapper } from './nats-wrapper';
+
+const start = async () => {
+  // check before application start environment variables get defined correctly
+  if (!process.env.NATS_URL) {
+    throw new Error('NATS_URL is not defined');
+  }
+
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error('NATS_CLIENT_ID is not defined');
+  }
+
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error('NATS_CLUSTER_ID is not defined');
+  }
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on('close', () => {
+      console.log('NATS connection closed!');
+      process.exit();
+    });
+
+    process.on('SIGINT', () => {
+      // intercept
+      natsWrapper.client.close(); // call to close in line 13
+    });
+
+    process.on('SIGTERM', () => {
+      // terminate
+      natsWrapper.client.close(); // call to close in line 13
+    });
+  } catch (err) {
+    console.log('Error Pop-up ==> ' + err);
+  }
+};
+
+start();
